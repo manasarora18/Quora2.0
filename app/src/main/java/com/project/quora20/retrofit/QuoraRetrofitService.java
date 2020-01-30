06f76bce15082f9c09e4f6684dcf110a6ec5da12
Manas Arora <manasarora98@gmail.com>
2020-01-30 15:03:20 +0530

    Day 2 Renaming, homeAdapter,Login Layouts

--- app/src/main/java/com/project/quora20/retrofit/QuoraRetrofitService.java
+++ app/src/main/java/com/project/quora20/retrofit/QuoraRetrofitService.java
@@ -1,5 +1,11 @@
 package com.project.quora20.retrofit;
 
+import com.project.quora20.entity.AccessTokenRegisterResponse;
+import com.project.quora20.entity.CoAuthRequestDTO;
+import com.project.quora20.entity.Question;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -11,9 +17,11 @@ public interface QuoraRetrofitService {
 //    @GET("/login-service/profile/{userId}")
 //    Call<UserDTO> getUserProfile(@Path("userId") String userId);
 //
-//    @POST("/login-service/register")
-//    Call <AccessTokenDTO> addUser(@Body RegisterUser registerUser);
+    @POST("/auth/signup")
+    Call <AccessTokenRegisterResponse> addUser(@Body CoAuthRequestDTO coAuthRequestDTO);
 //
+    @GET("/questions/{userId}")
+    Call <List<Question>> getAllQuestions(@Path("userId")String userId);
 //    @POST("/login-service/login")
 //    Call<AccessTokenDTO> loginUser(@Body RegisterUser registerUser);
 }
