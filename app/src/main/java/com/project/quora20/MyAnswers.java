443b4062d2d3a921545231b7e4909d6156203eab
Manas Arora <manasarora98@gmail.com>
2020-01-31 16:53:38 +0530

    Day 3 AnswerPage,New Post,Landing Page,Renaming, Category, homeAdapter,Login Layouts

--- app/src/main/java/com/project/quora20/MyAnswers.java
+++ app/src/main/java/com/project/quora20/MyAnswers.java
@@ -4,28 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import com.project.quora20.adapter.MyAnswerAdapter;
 import com.project.quora20.entity.Answer;
 import com.project.quora20.retrofit.QuoraRetrofitService;
-import com.project.quora20.retrofit.RetrofitClientInstance;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MyAnswers extends AppCompatActivity implements MyAnswerAdapter.IAnswerCommunicator {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
+    private RecyclerView answerRecyclerView;
+    private RecyclerView.Adapter answerAdapter;
     private RecyclerView.LayoutManager myAnswerLayoutManager;
     List<Answer> answerList;
 
@@ -37,10 +27,10 @@ public class MyAnswers extends AppCompatActivity implements MyAnswerAdapter.IAns
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_answers);
 
-        recyclerView = findViewById(R.id.ans_myAnswersRecycler);
+        answerRecyclerView = findViewById(R.id.ans_myAnswersRecycler);
         myAnswerLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(myAnswerLayoutManager);
-        recyclerView.setHasFixedSize(true);
+        answerRecyclerView.setLayoutManager(myAnswerLayoutManager);
+        answerRecyclerView.setHasFixedSize(true);
     }
 
     @Override
